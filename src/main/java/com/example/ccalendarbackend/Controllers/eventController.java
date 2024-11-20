6275bf6c90bcc52675d751438b12744630ff5f91
JoaquinRodriguez <joaquinrodriguez1499@gmail.com>
2024-11-20 20:42:17 -0300

    Feat(New Endpoint and delete extra Documentation) Add a new Endpoint getEventById

--- src/main/java/com/example/ccalendarbackend/Controllers/eventController.java
+++ src/main/java/com/example/ccalendarbackend/Controllers/eventController.java
@@ -3,11 +3,6 @@ import com.example.ccalendarbackend.DTO.EventResponseDTO;
 import com.example.ccalendarbackend.Models.Event;
 import com.example.ccalendarbackend.Repository.EventRepository;
 import com.example.ccalendarbackend.Services.EventService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -30,20 +25,6 @@ public class eventController {
 
     @CrossOrigin
     @GetMapping("/getAllEvents")
-    @Operation(
-            summary = "Retrieve all events",
-            description = "Fetches a complete list of all events stored in the database. The response includes all event details."
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of events",
-                    content = @Content(
-                            mediaType = "application/json",
-                            examples = @ExampleObject(
-                                    value = "[{\"id\":1,\"title\":\"Event 1\",\"date\":\"2024-11-19\"}, {\"id\":2,\"title\":\"Event 2\",\"date\":\"2024-11-20\"}]"
-                            ))),
-            @ApiResponse(responseCode = "500", description = "Internal server error while retrieving events",
-                    content = @Content(mediaType = "application/json"))
-    })
     public ResponseEntity<?> getAllEvents(){
         List<Event> events = eventRepository.findAll();
         return ResponseEntity.ok(events);
@@ -57,5 +38,9 @@ public class eventController {
         List<EventResponseDTO> events = eventService.getAllEventsByIdUser(idUser);
         return ResponseEntity.ok(events);
     }
-
+    @CrossOrigin
+    @GetMapping("/getEventById/{userId}")
+    public ResponseEntity<?> getEventById(@PathVariable String userId){
+        return ResponseEntity.ok(eventRepository.getEventsByUserId(userId));
+    }
 }
