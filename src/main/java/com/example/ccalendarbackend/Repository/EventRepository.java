efb2a1cb2852c39dd78c7cd514fec493920f37dd
JoaquinRodriguez <joaquinrodriguez1499@gmail.com>
2024-11-19 21:01:27 -0300

    Feat(EventRepository) Description(Create a new Query to get events with attachtments and notificacions)

--- src/main/java/com/example/ccalendarbackend/Repository/EventRepository.java
+++ src/main/java/com/example/ccalendarbackend/Repository/EventRepository.java
@@ -14,5 +14,26 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
     @Query("SELECT e FROM Event e WHERE e.iduser = :userId")
     List<Event> findEventsByUserId(String userId);
 
+    @Query(value = """
+        SELECT 
+            e.idevent AS eventId,
+            e.title AS eventTitle,
+            e.hora AS eventTime,
+            e.day AS eventDay,
+            e.iduser AS eventUser,
+            GROUP_CONCAT(DISTINCT a.idattachments) AS attachmentIds,
+            GROUP_CONCAT(DISTINCT a.url) AS attachmentUrls,
+            GROUP_CONCAT(DISTINCT n.idnotification) AS notificationIds,
+            GROUP_CONCAT(DISTINCT n.type) AS notificationTypes
+        FROM 
+            event e
+        LEFT JOIN event_has_attachments eha ON e.idevent = eha.event_idevent
+        LEFT JOIN attachments a ON eha.attachments_idattachments = a.idattachments
+        LEFT JOIN event_has_notification ehn ON e.idevent = ehn.event_idevent
+        LEFT JOIN notification n ON ehn.notification_idnotification = n.idnotification
+        GROUP BY 
+            e.idevent, e.title, e.hora, e.day, e.iduser
+        """, nativeQuery = true)
+    List<Object[]> getAllEventsWithNotificationsAndAttachments();
 
 }
