05d8dad60cdf579e51ed0414ec19b556e069ae2c
AgusS-Git <anstaats@alumnos.unsada.edu.ar>
2024-11-19 21:26:29 -0300

    Feat (Eventservice) Create getallEvents to bring all information from events

--- src/main/java/com/example/ccalendarbackend/Services/EventService.java
+++ src/main/java/com/example/ccalendarbackend/Services/EventService.java
@@ -1,6 +1,7 @@
 package com.example.ccalendarbackend.Services;
 
 
+import com.example.ccalendarbackend.DTO.EventResponseDTO;
 import com.example.ccalendarbackend.Models.Event;
 import com.example.ccalendarbackend.Repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -30,5 +33,30 @@ public class EventService {
     public List<Event> getEventsByUserId(String userId) {
         return eventRepository.findEventsByUserId(userId);
     }
+
+    public List<EventResponseDTO> getAllEvents() {
+        List<Object[]> results = eventRepository.getAllEventsWithNotificationsAndAttachments();
+        List<EventResponseDTO> events = new ArrayList<>();
+
+        for (Object[] row : results) {
+            EventResponseDTO dto = new EventResponseDTO();
+            dto.setEventId(((Number) row[0]).longValue());
+            dto.setEventTitle((String) row[1]);
+            dto.setEventTime((String) row[2]);
+            dto.setEventDay((String) row[3]);
+            dto.setEventUser(((Number) row[4]).longValue());
+            dto.setAttachmentIds(convertToList((String) row[5]));
+            dto.setAttachmentUrls(convertToList((String) row[6]));
+            dto.setNotificationIds(convertToList((String) row[7]));
+            dto.setNotificationTypes(convertToList((String) row[8]));
+            events.add(dto);
+        }
+        return events;
+    }
+
+    private List<Long> convertToList(String concatenated) {
+        return concatenated != null ? Arrays.asList(concatenated.split(",")) : new ArrayList<>();
+    }
+
 
 }
